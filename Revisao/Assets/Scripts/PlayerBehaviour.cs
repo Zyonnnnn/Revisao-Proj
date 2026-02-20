629a109066bcb931964e4c81ac4e6da868aafcb5
Zyonn_ <76115068+Zyonnnnn@users.noreply.github.com>
2026-02-19 22:57:03 -0300

    Jump
    made the player jump and put some test mashes

--- Revisao/Assets/Scripts/PlayerBehaviour.cs
+++ Revisao/Assets/Scripts/PlayerBehaviour.cs
@@ -1,29 +1,59 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    Rigidbody rb;
+    [SerializeField] float moveSpeed, jumpForce;
     InputControls inputControls;
-    private Vector2 inputDirection => inputControls.Player.Move.ReadValue<Vector2>();
+    bool canJump;
+    bool jumping;
+    InputAction jump;
+    private Vector2 InputDirection => inputControls.Player.Move.ReadValue<Vector2>();
 
     private void Awake()
     {
         inputControls = new InputControls();
         inputControls.Enable();
     }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        jump = InputSystem.actions.FindAction("Jump");
+    }
 
     private void Update()
     {
-        float moveX = inputDirection.x * moveSpeed * Time.deltaTime;
-        float moveZ = inputDirection.y * moveSpeed * Time.deltaTime;
+        float moveX = InputDirection.x * moveSpeed * Time.deltaTime;
+        float moveZ = InputDirection.y * moveSpeed * Time.deltaTime;
         
         transform.Translate(moveX, 0 ,moveZ);
+
+        if (jump.WasPressedThisFrame() && canJump)
+        {
+            jumping = true;
+            canJump = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (jumping)
+        {
+            Debug.Log("eita preula");
+            rb.AddForce(new(0, jumpForce, 0), ForceMode.Impulse);
+            jumping = false;
+        }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-        print("porra");
+        if (collision.collider.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
     }
 
     private void OnDestroy()
